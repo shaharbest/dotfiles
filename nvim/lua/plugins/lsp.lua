86cb92680ffebf1805a62e11e8971ec01a5c7717
Shahar Best <bestshahar9@gmail.com>
2025-04-25 20:57:30 +0300

    Python works

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,105 +1,144 @@
 return {
-  -- Mason setup
+  -- Mason setup for managing LSPs and formatters/linters
   {
     "williamboman/mason.nvim",
-    lazy = false,
     config = function()
       require("mason").setup()
     end,
   },
 
-  -- mason-lspconfig setup for automatic installation of language servers
+  -- mason-lspconfig for easier LSP installation and management
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = { auto_install = true },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "bashls",
-          "html",
-          "jsonls",
-          "lua_ls",
-          "ts_ls",
-          "yamlls",
-        },
-        automatic_installation = true, -- Install automatically
-      })
-    end,
+    opts = {
+      ensure_installed = {
+        "bashls",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "ts_ls",
+        "yamlls",
+        "pylsp",
+        "ruff",
+      },
+      automatic_installation = true,
+    },
   },
 
   -- nvim-lspconfig setup
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
 
-      -- Define a table of language servers and their configurations
+      -- Define configurations for specific language servers
       local servers = {
         ts_ls = {
-          filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
           root_dir = lspconfig.util.root_pattern(".meteor", "package.json", ".git"),
           init_options = { preferences = { disableSuggestions = false } },
         },
-        jsonls = {},
-        bashls = {},
-        yamlls = {},
         html = {},
         lua_ls = {},
+        ruff = {},
+        jsonls = {},
+        yamlls = {},
+        bashls = {},
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+                mccabe = { enabled = false },
+                pylsp_mypy = { enabled = false },
+                pylsp_black = { enabled = false },
+                pylsp_isort = { enabled = false },
+              },
+            },
+          },
+        },
       }
 
       -- Setup all the servers dynamically
       for server, config in pairs(servers) do
         lspconfig[server].setup({
           capabilities = capabilities,
-          opts = config,
+          settings = config.settings, -- Use 'settings' consistently
+          filetypes = config.filetypes,
+          root_dir = config.root_dir,
+          init_options = config.init_options,
         })
       end
 
       -- Set key mappings for common LSP actions
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentation" })
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Show references" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { desc = "Rename" })
 
       -- Diagnostic key mappings
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {})
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostics" })
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Show diagnostics in quickfix" })
     end,
   },
 
-  -- null-ls for formatting
+  -- null-ls for formatting and linting (using mason-null-ls)
   {
     "nvimtools/none-ls.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    dependencies = {
+      "williamboman/mason.nvim", -- Ensure mason is a dependency
+      "nvimtools/none-ls-extras.nvim",
+      "jay-babu/mason-null-ls.nvim",
+    },
     config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          "checkmake",
+          "prettier",
+          "stylua",
+          "eslint_d",
+          "shfmt",
+          "ruff",
+        },
+        automatic_installation = true,
+      })
+
       local null_ls = require("null-ls")
+      local formatting = null_ls.builtins.formatting
+      local diagnostics = null_ls.builtins.diagnostics
 
       null_ls.setup({
         sources = {
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.stylua,
+          diagnostics.checkmake,
+          formatting.stylua,
+          require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
+          require("none-ls.formatting.ruff_format"),
+          formatting.prettier.with({
+            filetypes = { "html", "json", "yaml", "markdown" },
+            formatting.prettier.with({
+              filetypes = { "html", "json", "yaml", "markdown" },
+              extra_args = { "--use-tabs", "false", "--tab-width", "2" }, -- Assuming you want 2 spaces here as well
+            }),
+          }),
+          formatting.shfmt.with({ args = { "-i", "4" } }),
         },
       })
       -- Keybinding for manual formatting
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
-
-  -- mason-null-ls setup for installing Prettier and Stylua
-  {
-    "jay-babu/mason-null-ls.nvim",
-    dependencies = { "williamboman/mason.nvim", "nvimtools/none-ls.nvim" },
-    config = function()
-      require("mason-null-ls").setup({
-        ensure_installed = { "prettier", "stylua" },
-        automatic_installation = true,
-      })
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format buffer" })
     end,
   },
 }
