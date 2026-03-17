194f4c285dfc5a94d2677feabee81e485ff23621
Shahar Best <shaharb@selacloud.com>
2026-03-17 13:53:06 -0400

    Floating

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -14,7 +14,7 @@ return {
     },
     cmd = "Neogit",
     keys = {
-      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+      { "<leader>gg", "<cmd>Neogit kind=floating<cr>", desc = "Show Neogit UI" }
     }
   },
   {
