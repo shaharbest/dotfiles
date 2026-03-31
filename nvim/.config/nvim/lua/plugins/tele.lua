09e4009144b013557a7f1d6495d0d28fdfe638b8
Shahar Best <shaharb@selacloud.com>
2026-03-31 12:15:28 -0400

    comment hidden

--- nvim/.config/nvim/lua/plugins/tele.lua
+++ nvim/.config/nvim/lua/plugins/tele.lua
@@ -22,7 +22,8 @@ return {
       defaults = {
         vimgrep_arguments = {
           'rg', '--color=never', '--no-heading', '--with-filename',
-          '--line-number', '--column', '--smart-case', '--hidden',
+          '--line-number', '--column', '--smart-case',
+          -- '--hidden',
         },
         preview = { treesitter = false },
         color_devicons = true,
