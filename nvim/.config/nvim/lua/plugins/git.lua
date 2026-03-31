70bb8ffe6882616400b61128b7bb9cfd7a22a080
Shahar Best <shaharb@selacloud.com>
2026-03-31 13:36:55 -0400

    Neogit GG key

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -14,7 +14,7 @@ return {
     },
     cmd = "Neogit",
     keys = {
-      { "<leader>gg", "<cmd>Neogit kind=floating<cr>", desc = "Show Neogit UI" }
+      { "<leader>GG", "<cmd>Neogit kind=floating<cr>", desc = "Show Neogit UI" }
     }
   },
   {
