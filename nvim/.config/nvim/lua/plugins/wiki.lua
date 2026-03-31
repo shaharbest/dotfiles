93a0ef2cd28e2009a623953555abc144bec7ee26
Shahar Best <shaharb@selacloud.com>
2026-03-31 12:21:49 -0400

    Wiki

---
+++ nvim/.config/nvim/lua/plugins/wiki.lua
@@ -0,0 +1,17 @@
+return {
+  "echaya/neowiki.nvim",
+  opts = {
+    wiki_dirs = {
+      -- neowiki.nvim supports both absolute and tilde-expanded paths
+      -- { name = "Work",     path = "~/work/wiki" },
+      -- { name = "Personal", path = "personal/wiki" },
+      { name = "Work",     path = "~/dev/wiki" },
+      -- { name = "Personal", path = "~/Documents/wiki" },
+    },
+  },
+  keys = {
+    { "<leader>ww", "<cmd>lua require('neowiki').open_wiki()<cr>",          desc = "Open Wiki" },
+    { "<leader>wW", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki in Floating Window" },
+    { "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>",  desc = "Open Wiki in Tab" },
+  },
+}
